a0753b046d9d57e7d169047a00e797d4d9d4dfb3
magicboy <64063910+magicboy0105@users.noreply.github.com>
2022-06-28 00:27:12 +0800

    Delete 02第2章  Python使用入门 directory

--- 02第2章  Python使用入门/ex2_30.py
+++
@@ -1,8 +0,0 @@
-#程序文件ex2_30.py
-import numpy as np
-a = np.array([[0, 3, 4], [1, 6, 4]])
-b = a.sum()   #使用方法，求矩阵所有元素的和
-c1 = sum(a)   #使用内置函数，求矩阵逐列元素的和
-c2 = np.sum(a, axis=0) #使用函数，求矩阵逐列元素的和
-c3 = np.sum(a, axis=0, keepdims=True)   #逐列求和
-print(c2.shape, c3.shape)  #c2是(3,)数组，c3是(1,3)数组
